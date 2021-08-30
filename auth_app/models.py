657b1c391d68e25966c32c65013081d1f20a25e5
Syed Rifat Ahsan <syed.rifat.ahsan@gmail.com>
2021-08-30 13:31:04 +0600

    Overridden Django's default user model

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=200)
+    username = None
+    groups=None
+    user_permissions=None
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
+    class Meta:
+        db_table = "auth_user"
+        unique_together = ('email', 'name',)
