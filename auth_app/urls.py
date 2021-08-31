8b7b61615cd250998ace97b9424f7a6fce594af2
Syed Rifat Ahsan <syed.rifat.ahsan@gmail.com>
2021-08-31 15:56:24 +0600

    Added functionality to verify token

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from auth_app import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
     path("user/", views.UserView.as_view()),
     path("user/<int:id>/", views.UserView.as_view()),
     path("user/auth/", views.LoginView.as_view()),
     path("user/logout/", views.LogoutView.as_view()),
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
