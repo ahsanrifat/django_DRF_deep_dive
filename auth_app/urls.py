e8bdf73f162af676c24698747eb11be353704fa4
Syed Rifat Ahsan <syed.rifat.ahsan@gmail.com>
2021-08-31 13:21:21 +0600

    Retrieved the user back from session jwt token

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from auth_app import views
 
 urlpatterns = [
-    path('user/', views.UserView.as_view()),
-    path('user/<int:id>/', views.UserView.as_view()),
-    path('user/auth/', views.LoginView.as_view())
+    path("user/", views.UserView.as_view()),
+    path("user/<int:id>/", views.UserView.as_view()),
+    path("user/auth/", views.LoginView.as_view()),
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,52 +1,68 @@
 from auth_app.models import User
 from django.http.response import JsonResponse
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer, TokenUserSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
-import jwt,datetime
+import jwt, datetime
 
 
 class LoginView(APIView):
-    def post(self,request):
-        email=request.data['email']
-        password=request.data['password']
-        user=User.objects.filter(email=email).first()
+    def post(self, request):
+        email = request.data["email"]
+        password = request.data["password"]
+        user = User.objects.filter(email=email).first()
         if user is None:
             raise AuthenticationFailed("Authentication Error")
         if not user.check_password(password):
             raise AuthenticationFailed("Authentication Error")
-        payload={
-            "id":user.id,
-            "exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=60),
-            "iat":datetime.datetime.utcnow()
+        payload = {
+            "id": user.id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            "iat": datetime.datetime.utcnow(),
         }
-        token=jwt.encode(payload,'secret',algorithm="HS256")
-        response=Response()
-        response.set_cookie(key='token',value=token,httponly=True)
-        response.data={"token":token}
+        token = jwt.encode(payload, "secret", algorithm="HS256")
+        response = Response()
+        response.set_cookie(key="token", value=token, httponly=True)
+        response.data = {"token": token}
         return response
-    
+
+    def get(self, request):
+        token = request.COOKIES.get("token")
+        if not token:
+            raise AuthenticationFailed("Unauthenticated token")
+        try:
+            payload = jwt.decode(token, "secret", algorithms="HS256")
+        except Exception as e:
+            raise AuthenticationFailed("Unauthenticated payload")
+        user = User.objects.filter(id=payload["id"]).first()
+        serializer = TokenUserSerializer(user)
+        return Response(serializer.data)
+
+
 class UserView(APIView):
-    def post(self,request):
+    def post(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    def get(self,request):
+
+    def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return JsonResponse(serializer.data, safe=False)
-    def put(self,request,id):
-        user=User.objects.get(id=id)
-        serializer = UserSerializer(user,data=request.data)
+
+    def put(self, request, id):
+        user = User.objects.get(id=id)
+        serializer = UserSerializer(user, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    def delete(self,request,id):
+
+    def delete(self, request, id):
         try:
-            user=User.objects.get(id=id)
+            user = User.objects.get(id=id)
             user.delete()
-            return Response({"message":True})
+            return Response({"message": True})
         except Exception as e:
-            return Response({"message":False})
+            return Response({"message": False})
